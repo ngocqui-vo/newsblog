629c11cc4ec39c32ed6b5a99a842e338b680ecb8
qui <ngocquivo11@gmail.com>
2024-10-04 01:22:26 +0700

    fix

--- core/settings.py
+++ core/settings.py
@@ -176,9 +176,6 @@ CKEDITOR_CONFIGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-print(EMAIL_HOST_USER)
-print(EMAIL_HOST_PASSWORD)
-print(EMAIL_PORT)
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "dzlama101@gmail.com"
+EMAIL_HOST_PASSWORD = "wucl fqha ikxp equw"

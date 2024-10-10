929f2483ad5dbeae9bf161ea4eb635b8b41e1e97
qui <ngocquivo11@gmail.com>
2024-10-11 03:40:39 +0700

    change permission for user post

--- account/views.py
+++ account/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth import authenticate
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from blog.models import Post
 
 from .forms import SignUpForm
 from .models import Customer
@@ -36,12 +39,21 @@ def user_register(request):
         forms = SignUpForm(request.POST)
 
         if forms.is_valid():
-            user = forms.save()
+            user = forms.save(commit=False)
+            user.is_staff = True
+            user.save()
             first_name = forms.cleaned_data.get('first_name')
             last_name = forms.cleaned_data.get('last_name')
             email = forms.cleaned_data.get('email')
             customer = Customer.objects.create(first_name=first_name, last_name=last_name, email=email, user=user)
             customer.save()
+            # Gán quyền CRUD cho model Post
+            content_type = ContentType.objects.get_for_model(Post)  # Lấy ContentType cho model Post
+            permissions = Permission.objects.filter(
+                content_type=content_type)  # Lấy tất cả các quyền liên quan đến model Post
+
+            # Gán các quyền CRUD cho user
+            user.user_permissions.add(*permissions)
             return redirect('user_login')
         else:
             messages.error(request, 'User already exist!!!')
--- blog/admin.py
+++ blog/admin.py
@@ -8,12 +8,57 @@ class PostAdmin(admin.ModelAdmin):
     list_display_links = ('id',)
     search_fields = ('title', 'category__title')
     ordering = ('title', 'created_at')
-    list_editable = ('title',)
+    exclude = ('user',)  # Loại bỏ user khỏi form cho user thường
 
     def get_category_title(self, obj):
         return obj.category.title
     get_category_title.short_description = 'Category'
 
+    # Chỉ hiển thị các post của chính user
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+    # Quyền chỉnh sửa post
+    def has_change_permission(self, request, obj=None):
+        if obj is not None and not request.user.is_superuser:
+            return obj.user == request.user
+        return True
+
+    # Quyền xóa post
+    def has_delete_permission(self, request, obj=None):
+        if obj is not None and not request.user.is_superuser:
+            return obj.user == request.user
+        return True
+
+    # Chỉ cho phép thêm post nếu user đã đăng nhập
+    def has_add_permission(self, request):
+        return request.user.is_authenticated
+
+    # Ẩn các model khác đối với user thường
+    def get_model_perms(self, request):
+        if request.user.is_superuser:
+            return super().get_model_perms(request)
+        return {
+            'add': True,
+            'change': True,
+            'delete': True,
+        }
+
+    # Chỉnh sửa form: Admin có thể chỉnh sửa `views`, user thường không thể
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if not request.user.is_superuser:
+            form.base_fields['views'].disabled = True  # Vô hiệu hóa trường 'views' cho user thường
+        return form
+
+    # Gán user hiện tại cho post khi lưu
+    def save_model(self, request, obj, form, change):
+        if not request.user.is_superuser:
+            obj.user = request.user  # Gán user hiện tại vào post
+        super().save_model(request, obj, form, change)
 
 # Register your models here.
 admin.site.register(Category)
